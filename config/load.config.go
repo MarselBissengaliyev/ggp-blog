f91711d81e23b7e1fa561859420a43494cef6902
Marsel Bissengaliyev <marselbisengaliev1@gmail.com>
2023-09-10 18:02:22 +0500

    create: create function for load configuration
    - Create config/load.config.go file
    - Create LoadConfig function
    - Create Config struct
    - Import "github.com/spf13/viper"
    
    Signed-off-by: MarselBissengaliyev <marselbisengaliev@gmail.com>

---
+++ config/load.config.go
@@ -0,0 +1,30 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DB_Host     string `mapstructure:"DB_HOST"`
+	DB_Password string `mapstructure:"DB_PASSWORD"`
+	DB_User     string `mapstructure:"DB_USER"`
+	DB_Name     string `mapstructure:"DB_NAME"`
+	DB_SSLMode  string `mapstructure:"DB_SSL_MODE"`
+	DB_Port     string `mapstructure:"DB_PORT"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigType("env")
+	viper.SetConfigName("app")
+
+	viper.AutomaticEnv()
+
+	if err = viper.ReadInConfig(); err != nil {
+		return
+	}
+
+	if err = viper.Unmarshal(&config); err != nil {
+		return
+	}
+
+	return
+}
