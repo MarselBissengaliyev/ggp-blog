6b33fcf9b765fd186536a66134e19c6b22387313
Marsel Bissengaliyev <marselbisengaliev1@gmail.com>
2023-09-11 21:27:37 +0500

    create new connection handler

---
+++ storage/postgres.go
@@ -0,0 +1,28 @@
+package storage
+
+import (
+	"fmt"
+
+	"github.com/MarselBissengaliyev/ggp-blog/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewConnection(config *config.Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		config.DB_Host,
+		config.DB_User,
+		config.DB_Password,
+		config.DB_Name,
+		config.DB_Port,
+		config.DB_SSLMode,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return db, err
+	}
+
+	return db, nil
+}
