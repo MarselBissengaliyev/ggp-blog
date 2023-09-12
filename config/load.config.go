1dde796e31b99a6f105d698ff91d996ad1749e05
Marsel Bissengaliyev <marselbisengaliev1@gmail.com>
2023-09-12 22:04:36 +0500

    update config struct

--- config/load.config.go
+++ config/load.config.go
@@ -3,12 +3,13 @@ package config
 import "github.com/spf13/viper"
 
 type Config struct {
-	DB_Host     string `mapstructure:"DB_HOST"`
-	DB_Password string `mapstructure:"DB_PASSWORD"`
-	DB_User     string `mapstructure:"DB_USER"`
-	DB_Name     string `mapstructure:"DB_NAME"`
-	DB_SSLMode  string `mapstructure:"DB_SSL_MODE"`
-	DB_Port     string `mapstructure:"DB_PORT"`
+	DB_Host      string `mapstructure:"DB_HOST"`
+	DB_Password  string `mapstructure:"DB_PASSWORD"`
+	DB_User      string `mapstructure:"DB_USER"`
+	DB_Name      string `mapstructure:"DB_NAME"`
+	DB_SSLMode   string `mapstructure:"DB_SSL_MODE"`
+	DB_Port      string `mapstructure:"DB_PORT"`
+	Token_Secret string `mapstructure:"TOKEN_SECRET"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
