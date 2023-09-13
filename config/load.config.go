0ea49af849d19ee636963d9d028a7431a11e4e51
Marsel Bissengaliyev <marselbisengaliev1@gmail.com>
2023-09-13 15:46:00 +0500

    add client origin param in config  struct

--- config/load.config.go
+++ config/load.config.go
@@ -3,13 +3,19 @@ package config
 import "github.com/spf13/viper"
 
 type Config struct {
-	DB_Host      string `mapstructure:"DB_HOST"`
-	DB_Password  string `mapstructure:"DB_PASSWORD"`
-	DB_User      string `mapstructure:"DB_USER"`
-	DB_Name      string `mapstructure:"DB_NAME"`
-	DB_SSLMode   string `mapstructure:"DB_SSL_MODE"`
-	DB_Port      string `mapstructure:"DB_PORT"`
-	Token_Secret string `mapstructure:"TOKEN_SECRET"`
+	DB_Host         string `mapstructure:"DB_HOST"`
+	DB_Password     string `mapstructure:"DB_PASSWORD"`
+	DB_User         string `mapstructure:"DB_USER"`
+	DB_Name         string `mapstructure:"DB_NAME"`
+	DB_SSLMode      string `mapstructure:"DB_SSL_MODE"`
+	DB_Port         string `mapstructure:"DB_PORT"`
+	Token_Secret    string `mapstructure:"TOKEN_SECRET"`
+	Email_From      string `mapstructure:"EMAIL_FROM"`
+	Email_Smtp_Host string `mapstructure:"EMAIL_SMTP_HOST"`
+	Email_Smtp_User string `mapstructure:"EMAIL_SMTP_USER"`
+	Email_Smtp_Pass string `mapstructure:"EMAIL_SMTP_PASS"`
+	Email_Smtp_Port int    `mapstructure:"EMAIL_SMTP_PORT"`
+	Client_Origin   string `mapstructure:"CLIENT_ORIGIN"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
