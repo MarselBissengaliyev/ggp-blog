02fb6b5c51776370c31dc6f8faf7e847c5e02e64
Marsel Bissengaliyev <marselbisengaliev1@gmail.com>
2023-09-18 09:37:27 +0500

    create user routes

--- routes/user.route.go
+++ routes/user.route.go
@@ -1,13 +1,18 @@
 package routes
 
 import (
+	"github.com/MarselBissengaliyev/ggp-blog/middlewares"
+	"github.com/MarselBissengaliyev/ggp-blog/repositories"
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
 
-func RegisterUserStoreRoutes(rg *gin.RouterGroup, db *gorm.DB) {
+func RegisterUserStoreRoutes(rg *gin.RouterGroup, r *repositories.Repository) {
 	routes := rg.Group("/users")
 
-	routes.GET("/")
-	routes.GET("/:username")
+	routes.GET("/", r.GetUsers)
+	routes.GET("/:user_name", r.GetUserByUsername)
+
+	privateRoutes := routes.Group("/", middlewares.Authenticate(r))
+	privateRoutes.PUT("/:user_name", r.UpdateUserByUsername)
+	privateRoutes.DELETE("/:user_name", r.DeleteUserByUsername)
 }
