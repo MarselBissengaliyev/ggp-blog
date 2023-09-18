331608b6ed611bb9b76742f135eabb7458627afc
Marsel Bissengaliyev <marselbisengaliev1@gmail.com>
2023-09-18 09:34:01 +0500

    create update and delete user handler

--- repositories/user.repository.go
+++ repositories/user.repository.go
@@ -1,12 +1,15 @@
 package repositories
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/MarselBissengaliyev/ggp-blog/models"
+	"github.com/MarselBissengaliyev/ggp-blog/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/thanhpk/randstr"
 )
 
 func (r *Repository) GetUsers(c *gin.Context) {
@@ -69,7 +72,7 @@ func (r *Repository) GetUserByUsername(c *gin.Context) {
 		return
 	}
 
-	if err := r.DB.First(&user, fmt.Sprintf("user_name = %s", userName)).Error; err != nil {
+	if err := r.DB.First(&user, fmt.Sprintf("user_name = '%s'", userName)).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"status":  "failed",
 			"error":   err.Error(),
@@ -84,4 +87,173 @@ func (r *Repository) GetUserByUsername(c *gin.Context) {
 		"data":    user,
 		"message": "you succefully got user by user_name",
 	})
+}
+
+func (r *Repository) UpdateUserByUsername(c *gin.Context) {
+	var user models.User
+	var foundUser models.User
+	var count int64
+
+	uid := fmt.Sprint(c.Keys["uid"])
+
+	if uid == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "failed",
+			"error":   "uid cannot be emtpy string",
+			"message": "error occured while reading uid key",
+		})
+
+		return
+	}
+
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "failed",
+			"error":   err.Error(),
+			"message": "error occured while binding json",
+		})
+
+		return
+	}
+
+	if err := r.DB.First(&foundUser, fmt.Sprintf("id = '%s'", uid)).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"status":  "failed",
+			"error":   err.Error(),
+			"message": "error occured while finding user",
+		})
+
+		return
+	}
+
+	r.DB.Model(&models.User{}).Where("email = ?", user.Email).Count(&count)
+
+	if count > 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "failed",
+			"error":   "this email already exists",
+			"message": "error occured while creating account",
+		})
+		return
+	}
+
+	r.DB.Model(&models.User{}).Where("user_name = ?", user.UserName).Count(&count)
+
+	if count > 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "failed",
+			"error":   "this username already exists",
+			"message": "error occured while creating account",
+		})
+		return
+	}
+
+	if uid != fmt.Sprint(foundUser.ID) {
+		c.JSON(http.StatusForbidden, gin.H{
+			"status":  "failed",
+			"error":   "you don't have permission to access this",
+			"message": "error occured while checking user permission",
+		})
+
+		return
+	}
+
+	password := utils.HashPassword(user.Password)
+	user.Password = password
+
+	if foundUser.Email != user.Email {
+		code := randstr.String(20)
+		user.EmailCode = sql.NullString{String: utils.Encode(code), Valid: true}
+		user.EmailConfirmed = false
+
+		emailData := utils.EmailData{
+			URL:       r.Config.App_Origin + "/auth/confirm-email/" + user.EmailCode.String,
+			FirstName: user.FirstName,
+			Subject:   "Your account verification code",
+		}
+
+		utils.SendEmail(&user, &emailData, r.Config)
+
+		message := "you succefully update user. We sent an email with a verification code to " + user.Email
+
+		if err := r.DB.Where("id = ?", uid).Updates(&user).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  "failed",
+				"error":   err.Error(),
+				"message": "error occured while update user",
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"status":  "success",
+			"data":    user,
+			"message": message,
+		})
+
+		return
+	}
+
+	if err := r.DB.Where("id = ?", uid).Updates(&user).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "failed",
+			"error":   err.Error(),
+			"message": "error occured while update user",
+		})
+
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  "success",
+		"data":    user,
+		"message": "you succefully update user",
+	})
+}
+
+func (r *Repository) DeleteUserByUsername(c *gin.Context) {
+	var user models.User
+
+	uid := fmt.Sprint(c.Keys["uid"])
+
+	if uid == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "failed",
+			"error":   "uid cannot be emtpy string",
+			"message": "error occured while reading uid key",
+		})
+
+		return
+	}
+
+	if err := r.DB.First(&user, fmt.Sprintf("id = '%s'", uid)).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"status":  "failed",
+			"error":   err.Error(),
+			"message": "error occured while finding user",
+		})
+
+		return
+	}
+
+	if uid != fmt.Sprint(user.ID) {
+		c.JSON(http.StatusForbidden, gin.H{
+			"status":  "failed",
+			"error":   "you don't have permission to access this",
+			"message": "error occured while checking user permission",
+		})
+
+		return
+	}
+
+	if err := r.DB.Delete(&user, fmt.Sprintf("id = %s", uid)).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "failed",
+			"error":   err.Error(),
+			"message": "error occured while dekete user",
+		})
+
+		return
+	}
 }
