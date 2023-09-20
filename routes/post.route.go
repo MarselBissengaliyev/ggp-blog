7a11a20ae24b956dedd9758e27e299ca90874b62
Marsel Bissengaliyev <marselbisengaliev1@gmail.com>
2023-09-20 22:45:46 +0500

    add post reaction routes

--- routes/post.route.go
+++ routes/post.route.go
@@ -12,9 +12,15 @@ func RegisterPostStoreRoutes(rg *gin.RouterGroup, r *repositories.Repository) {
 	routes.GET("/", r.GetPosts)
 	routes.GET("/:slug", r.GetPostBySlug)
 
+	routes.GET("/:slug/reactions", r.GetPostReactions)
+
 	privateRoutes := routes.Group("/", middlewares.Authenticate(r))
 	
 	privateRoutes.POST("/", r.CreatePost)
 	privateRoutes.PUT("/:slug", r.UpdatePostBySlug)
 	privateRoutes.DELETE("/:slug", r.DeletePostBySlug)
+
+	privateRoutes.POST("/:slug/reactions/", r.CreatePostReaction)
+	privateRoutes.PUT("/:slug/reactions/:id", r.UpdatePostReaction)
+	privateRoutes.DELETE("/:slug/reactions/:id", r.DeletePostReaction)
 }
